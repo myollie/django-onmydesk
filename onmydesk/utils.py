6b37ffcdcce00275ae71465b22e446a8a39c4de3
Alisson R. Perez <alissonperez@gmail.com>
2016-05-06 19:35:08 -0300

    Add function str_to_date

--- onmydesk/utils.py
+++ onmydesk/utils.py
@@ -1,4 +1,6 @@
 """Module with common utilities to this package"""
+import re
+from datetime import timedelta
 
 
 def my_import(class_name):
@@ -19,3 +21,27 @@ def my_import(class_name):
     mod = __import__('.'.join(packs), fromlist=[class_name])
     klass = getattr(mod, class_name)
     return klass
+
+
+def str_to_date(value, reference_date):
+    '''
+    Convert a string like 'D-1' to a "reference_date - timedelta(days=1)"
+
+    :param str value: String like 'D-1', 'D+1', 'D'...
+    :param date reference_date: Date to be used as 'D'
+    :return: Result date
+    '''
+
+    n_value = value.strip(' ').replace(' ', '').upper()
+
+    if not re.match('^D[\-+][0-9]+$|^D$', n_value):
+        raise ValueError('Wrong value "{}"'.format(value))
+
+    if n_value == 'D':
+        return reference_date
+    elif n_value[:2] == 'D-':
+        days = int(n_value[2:])
+        return reference_date - timedelta(days=days)
+    elif n_value[:2] == 'D+':
+        days = int(n_value[2:])
+        return reference_date + timedelta(days=days)
