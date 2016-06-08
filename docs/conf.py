967b66d0d05a8b417d10b4ef144603ffbec83668
Alisson R. Perez <alissonperez@gmail.com>
2016-06-08 16:26:10 -0300

    Change theme to readthedocs and change copyright

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Django - OnMyDesk'
-copyright = '2016, Alisson R. Perez (at Knowledge4Life)'
+copyright = '2016, Knowledge 4 Life'
 author = 'Alisson R. Perez (at Knowledge4Life)'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -118,7 +118,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
